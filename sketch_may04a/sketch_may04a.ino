3a53b3f16bf023a3e435cbde4b97af99587b445e
vladdsm <vladimir.zhbanko@gmail.com>
2021-06-04 23:18:21 +0200

    function to be fixed

--- sketch_may04a/sketch_may04a.ino
+++ sketch_may04a/sketch_may04a.ino
@@ -51,9 +51,9 @@ int Psegm = 0; //Segment position
 
 void loop() { 
   Yaxis = analogRead(A0)/128; //value from 0 to 7, default 4
-  Serial.print(" YAxis: "); Serial.print(Yaxis);
+  //Serial.print(" YAxis: "); Serial.print(Yaxis);
   Xaxis = analogRead(A1)/128; //value from 0 to 7, default 3
-  Serial.print(" XAxis: "); Serial.print(Xaxis);
+  //Serial.print(" XAxis: "); Serial.print(Xaxis);
   Psegm = jumpSegment(Xaxis);
   
   lc.setLed(Psegm,Xaxis,Yaxis,true);
@@ -76,13 +76,18 @@ int jumpSegment(int xpos){
  //@Purpose: Jump to next segment when keeping joystick on the edge of the module  
  //@Return: Function returns integer value from 0 to 3
  int result = 0;
+ /*
+ //moving right
  if(xpos < 1 && result == 0) result = 0;
+ if(xpos < 1 && result == 1) result = 0;
+ if(xpos < 1 && result == 2) result = 1;
+ if(xpos < 1 && result == 3) result = 2;
+ //moving left
  if(xpos > 6 && result == 0) result = 1;
  if(xpos > 6 && result == 1) result = 2;
  if(xpos > 6 && result == 2) result = 3;
  if(xpos > 6 && result == 3) result = 3;
- 
- 
+ */
  return(result); 
 
   
