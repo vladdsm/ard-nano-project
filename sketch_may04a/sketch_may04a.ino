0012f250d15ac0abc93bf406ac00fea090f569fe
vladdsm <vladimir.zhbanko@gmail.com>
2021-06-06 17:37:26 +0200

    more comments

--- sketch_may04a/sketch_may04a.ino
+++ sketch_may04a/sketch_may04a.ino
@@ -1,10 +1,8 @@
 //+------------------------------------------------------------------------------+
-//|                                                     Punkt auf Matrix bewegen:|
+//|                                                    Fussball Cup 'GymThun2021'|
 //|                                             Copyright 2021,Viacheslav Zhbanko| 
 //|                                                            Gymnasium Thun G23|
 //+------------------------------------------------------------------------------+
-//Include the library to control LED see http://wayoda.github.io/LedControl/pages/software
-//
 //============================================
 // Hardware definition
 //============================================
@@ -37,6 +35,7 @@ axis     X
 //============================================
 // Include libraries
 //============================================
+//Library to control LED see http://wayoda.github.io/LedControl/pages/software
 #include "LedControl.h"
 //============================================
 // Properties definition
@@ -50,16 +49,15 @@ axis     X
  We have 4 cascaded modules MAX72XX.
  */
 LedControl lc=LedControl(12,11,10,4);
-
 //============================================
 // Global Variables definition
 //============================================
 int Xaxis = 0; //Joystick X Asis
 int Yaxis = 0; //Joystick Y Axis
-int Psegm = 0; //Segment position
+int Psegm = 2; //Segment position
 int Xpos  = 0; //Position in X axis
 int Ypos  = 0; //Position in Y axis
-int Xdir  = 7; //Direction in X axis
+int Xdir  = 3; //Direction in X axis
 int Ydir  = 7; //Direction in Y axis
 int Tloop = 200;
 bool Opt_Serial = false; //option to enable serial monitor
@@ -67,12 +65,11 @@ bool Opt_ExLed = true;   //option to use extra led
 //============================================
 // Setup Module: code runs 1x at start
 //============================================
-
-
 void setup() {
-   
+  //initialize HW pins 
   pinMode(2,OUTPUT);         //set D2 pin as Output
   digitalWrite(2,HIGH);      //activate D2 to control Joystick
+  //set options
   if(Opt_ExLed) { pinMode(3,OUTPUT);
                   digitalWrite(3,HIGH);      //test to activate LED
                   delay(3000);
@@ -84,7 +81,7 @@ void setup() {
     lc.setIntensity(i,2);
     lc.clearDisplay(i);
   }
-  //create doors
+  //create doors in playground
   makeDoors();
   
 }
@@ -92,17 +89,20 @@ void setup() {
 // End of Initialization Module
 //============================================
 
-
-
 //============================================
 // Loop Module: code runs continuously
 //============================================
-
 void loop() { 
+  
+  //Refresh status of Joystick
   Xaxis = analogRead(A0)/128; //value from 0 to 7, default 3
-  //Serial.println(" XAxis: "); Serial.print(Xaxis);
   Yaxis = analogRead(A1)/128; //value from 0 to 7, default 4
-  //Serial.println(" YAxis: "); Serial.print(Yaxis);
+  
+  //Enable diagnostics via Serial Print (top right corner)
+  if(Opt_Serial){
+    Serial.println(" XAxis: "); Serial.print(Xaxis);
+    //Serial.println(" YAxiSerial.print(Yaxis);
+  }
   
   //Change position of point if needed
   Xdir = moveDir(Xaxis, Xdir);
@@ -111,7 +111,7 @@ void loop() {
   //Change position of segment if needed
   Psegm = jumpSegment(Yaxis, Psegm);
   
-  //set state of LED
+  //Set state of LED
   lc.setLed(Psegm,Xdir,Ydir,true);
   //delay
   delay(Tloop);
@@ -120,7 +120,7 @@ void loop() {
   //check of goal
   litLed(Xdir, Ydir, Psegm);
 
-  //create doors
+  //Refresh doors
   makeDoors();
   
 }
@@ -133,11 +133,15 @@ void loop() {
 //============================================
 // User Defined Functions
 //============================================
+
+//-------------
+// Function: jumpSegment(int xpos, int segment)
+//-------------
 int jumpSegment(int xpos, int segment){
- //@Purpose: Jump to next segment when keeping joystick on the edge of the module  
- //@xpos    integer, input from Joystic
- //@segment integer, provided current position of segment
- //@Return: Function returns integer value from 0 to 3
+ //@Purpose: Jump to next segment when directing joystick on Y axis  
+ //@xpos    integer, input from Joystic, typically from 0 to 7
+ //@segment integer, provided current position of segment, typically from 0 to 3
+ //@Return: Value from 0 to 3
  int result = 0;
  
  //moving right
@@ -155,10 +159,15 @@ int jumpSegment(int xpos, int segment){
  return(result); 
 
 }
-
+//-------------
+// End Of Function: jumpSegment(int xpos, int segment)
+//-------------
+//-------------
+// Function: moveDir (int AxPos, int oldpos)
+//-------------
 int moveDir (int AxPos, int oldpos) {
- //@Purpose: defines new position of point on X axis based on Joystic position and old position
- //@AxPos  integer, input from Joystic
+ //@Purpose: defines new position of point on X or Y axis based on Joystick position and old position
+ //@AxPos  integer, input from Joystic, typically from 0 to 7 
  //@oldpos integer, provided old position
  //@Return: Function returns integer value from 0 to 7
  int result = 7;
@@ -169,35 +178,49 @@ int moveDir (int AxPos, int oldpos) {
  else result = oldpos;
  return(result);
 }
-
+//-------------
+// End of Function: moveDir (int AxPos, int oldpos)
+//-------------
+//-------------
+// Function: litLed(int xPos, int yPos, int zSegm)
+//-------------
 void litLed(int xPos, int yPos, int zSegm) {
  //@Purpose: change status of extra LED 
  //@xPos  integer, position on x axis
  //@yPos  integer, position on y axis
  //@zSegm integer, position of segment
- //@Return: Function lit LED only when both positions are on extreme edges
+ //@Return: Lit LED only when point is on extreme edges and within doors
   if(zSegm == 0) {
-      if((xPos >= 2 || xPos <= 5) && yPos == 7) {digitalWrite(3,HIGH);} 
+      if((xPos >= 2 && xPos <= 5) && yPos == 7) {digitalWrite(3,HIGH);} else {digitalWrite(3,LOW);} 
     } else if(zSegm == 3) {
-      if((xPos >= 2 || xPos <= 5) && yPos == 0) {digitalWrite(3,HIGH);} 
+      if((xPos >= 2 && xPos <= 5) && yPos == 0) {digitalWrite(3,HIGH);} else {digitalWrite(3,LOW);}
     } else {digitalWrite(3,LOW);}
    
   }
-
+//-------------
+// End of Function: litLed(int xPos, int yPos, int zSegm)
+//-------------
+//-------------
+// Function: makeDoors()
+//-------------
 void makeDoors(){
- //@Purpose: creates doors on the edges of LED playzone 
- //@Return: Function lit LED to represent doors
-     //set state of LED
+ //@Purpose: Creates doors on the edges of LED playzone 
+ //@Return:  Function lit LED to represent doors
+  //Door on segment 0
   lc.setLed(0,7,7,true);
   lc.setLed(0,6,7,true);
   lc.setLed(0,1,7,true);
   lc.setLed(0,0,7,true);
-
+  //Door on segment 3
   lc.setLed(3,7,0,true);
   lc.setLed(3,6,0,true);
   lc.setLed(3,1,0,true);
   lc.setLed(3,0,0,true);
   }  
+//-------------
+// End of Function: makeDoors()
+//-------------
+  
 //============================================
 // End Of User Defined Functions
 //============================================
