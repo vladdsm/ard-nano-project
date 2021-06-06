ac63827e1f23e92f07461a9306d436a6f3addbb2
vladdsm <vladimir.zhbanko@gmail.com>
2021-06-06 16:16:13 +0200

    moving correctly in one segment

--- sketch_may04a/sketch_may04a.ino
+++ sketch_may04a/sketch_may04a.ino
@@ -9,16 +9,16 @@
 // Hardware definition
 //============================================
 /*
-   LCD Panel
-      §ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§
- =====§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§
- =====§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§
- =====§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§
- =====§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§
- =====§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§
-      §ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§
-      §ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§
-
+   LCD Panel                                    X
+      §ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§     7
+ =====§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§     6
+ =====§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§     5
+ =====§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§     4
+ =====§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§     3
+ =====§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§     2
+      §ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§     1
+      §ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§ΩΩΩΩΩΩΩΩ§     0
+    Y  76543210 76543210 76543210 76543210 
 segments  0         1        2        3
 ----------
 
@@ -29,9 +29,9 @@ segments  0         1        2        3
  =====§ΩΩΩΩΩΩΩΩ§
  =====§ΩΩΩΩΩΩΩΩ§
 
-axis   ◀ X ►
+axis   ◀ Y ►
          △
-axis     Y
+axis     X
          ▽
 */
 //============================================
@@ -59,8 +59,8 @@ int Yaxis = 0; //Joystick Y Axis
 int Psegm = 0; //Segment position
 int Xpos  = 0; //Position in X axis
 int Ypos  = 0; //Position in Y axis
-int Xdir  = 0; //Direction in X axis
-int Ydir  = 0; //Direction in Y axis
+int Xdir  = 7; //Direction in X axis
+int Ydir  = 7; //Direction in Y axis
 int Tloop = 200;
 bool Opt_Serial = false; //option to enable serial monitor
 bool Opt_ExLed = true;   //option to use extra led
@@ -107,7 +107,7 @@ void loop() {
   Ydir = moveDir(Yaxis, Ydir);
 
   //Change position of segment if needed
-  Psegm = jumpSegment(Xaxis, Psegm);
+  Psegm = jumpSegment(Yaxis, Psegm);
   
   //set state of LED
   lc.setLed(Psegm,Xdir,Ydir,true);
@@ -134,32 +134,32 @@ int jumpSegment(int xpos, int segment){
  //@Return: Function returns integer value from 0 to 3
  int result = 0;
  
- //moving right
- if(xpos == 0 && segment == 0) {result = 0; delay(100);} 
- else if(xpos == 0 && segment == 1) {result = 0; delay(100);}
- else if(xpos == 0 && segment == 2) {result = 1; delay(100);}
- else if(xpos == 0 && segment == 3) {result = 2; delay(100);}
- //moving left
- else if(xpos == 7 && segment == 0) {result = 1; delay(100);}
- else if(xpos == 7 && segment == 1) {result = 2; delay(100);}
- else if(xpos == 7 && segment == 2) {result = 3; delay(100);}
- else if(xpos == 7 && segment == 3) {result = 3; delay(100);}
- else result = segment;
+// //moving right
+// if(xpos == 0 && segment == 0) {result = 0; delay(100);} 
+// else if(xpos == 0 && segment == 1) {result = 0; delay(100);}
+// else if(xpos == 0 && segment == 2) {result = 1; delay(100);}
+// else if(xpos == 0 && segment == 3) {result = 2; delay(100);}
+// //moving left
+// else if(xpos == 7 && segment == 0) {result = 1; delay(100);}
+// else if(xpos == 7 && segment == 1) {result = 2; delay(100);}
+// else if(xpos == 7 && segment == 2) {result = 3; delay(100);}
+// else if(xpos == 7 && segment == 3) {result = 3; delay(100);}
+// else result = segment;
  
  return(result); 
 
 }
 
 int moveDir (int AxPos, int oldpos) {
- //@Purpose: defines new position of point on X or Y axis based on Joystic position and old position
+ //@Purpose: defines new position of point on X axis based on Joystic position and old position
  //@AxPos  integer, input from Joystic
  //@oldpos integer, provided old position
  //@Return: Function returns integer value from 0 to 7
- int result = 0;
- if(AxPos == 0 && oldpos == 0) {result = 0;}
- else if(AxPos == 7 && oldpos == 7) {result = 7;}
- else if(AxPos == 0) {result = oldpos - 1;}
- else if(AxPos == 7) {result = oldpos + 1;}
+ int result = 7;
+ if(AxPos == 0 && oldpos == 7) {result = 7;}
+ else if(AxPos == 7 && oldpos == 0) {result = 0;}
+ else if(AxPos == 0) {result = oldpos + 1;}
+ else if(AxPos == 7) {result = oldpos - 1;}
  else result = oldpos;
  return(result);
 }
